2d05dad4822623dc4c76934a3454bcbadfbdcba5
Dawid Klatkiewicz <klatkiewiczdawid@gmail.com>
2020-05-12 03:12:09 -0400

    Replaced commandLoader
    Signed-off-by: Dawid Klatkiewicz <klatkiewiczdawid@gmail.com>

--- commandHandler.js
+++ commandHandler.js
@@ -1,18 +1,14 @@
 'use strict'
 
-const permFinder = require('./lib/permFinder.js');
+const ParentCommand = require('./lib/parentcommand.js');
 
-let commandMap;
-
-require('./commandLoader.js')
-.then(value => {
-   commandMap = value;
-});
-
-class CommandHandler {
+class CommandHandler extends ParentCommand {
 
    constructor(api) {
+      super('commands');
+      this.title = '';
       this.api = api;
+      this.load();
    }
 
    parse(cmdString) {
@@ -26,30 +22,12 @@ class CommandHandler {
    }
 
    handle(parsed, message) {
-      //no command 
+
+      // no command
       if (parsed.cmd.trim() == '') return;
 
-      //TODO: replace with parentcommand
-
-      try {
-         const permLevel = permFinder.byMessage(message, this.api.settingsManager);
-         if (commandMap[parsed.cmd] && permLevel < commandMap[parsed.cmd].permissions && permLevel != -1) {
-            message.reply(`You don't have permission to run that command. Required: ${commandMap[parsed.cmd].permissions}, Yours: ${permLevel} `);
-            return;
-         }
-
-         const result = commandMap[parsed.cmd].payload({...this.api, message, commandMap}, parsed.args);
-         if (result === 2) {
-            const prefix = this.api.settingsManager.getAttribute('prefix');
-            message.channel.send(`Usage: ${prefix}${parsed.cmd} ${commandMap[parsed.cmd].usage || ''}`);
-         }
-         if (result === 1) {
-            message.channel.send(`There was a problem running that command.`);
-         } 
-      } catch (err) {
-         console.log(err);
-         message.channel.send(`Command "${parsed.cmd}" not found.`);
-      }
+      // handle
+      this.payload({...this.api, message, commandMap: this.commandMap}, [parsed.cmd, ...parsed.args]);
    }
 }
 
